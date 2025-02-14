18aba69680adee3f2de9f82fb8b3c1e7dc604cbd
Ayanangshu Dutta <ayanangshudutta1@gmail.com>
2025-02-14 11:52:55 +0530

    netlify toml

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1000, // Increase to 1MB
+    chunkSizeWarningLimit: 1000, // Increase to 1MB to avoid chunk size warnings
+    target: "esnext", // For modern JS compatibility
+    modulePreload: true, // For proper module handling
+  },
+  esbuild: {
+    jsx: "react", // Ensures JSX is properly transpiled for React
   },
 });
