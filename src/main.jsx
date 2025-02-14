d622123d6f368bee446a86df7d49b333f6ec29d1
Ayanangshu Dutta <ayanangshudutta1@gmail.com>
2025-02-14 11:23:49 +0530

    env added

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,15 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App.jsx";
 
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!clerkPubKey) {
+  throw new Error(
+    "Missing Clerk Publishable Key! Set VITE_CLERK_PUBLISHABLE_KEY in your environment variables."
+  );
+}
+
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey="PUBLIISHABLE_KEY">
+  <ClerkProvider publishableKey={clerkPubKey}>
     <App />
   </ClerkProvider>
 );
